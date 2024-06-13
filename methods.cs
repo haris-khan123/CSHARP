744b5b7f7225c6150dc0ee525834731a86d976fa
haris-khan123 <hariskhan94434@gmail.com>
2024-06-13 16:04:27 +0500

    this is my c sharp program

---
+++ methods.cs
@@ -0,0 +1,38 @@
+public class methods
+{
+
+    public class main(string[] args)
+    {
+
+Console.WriteLine("haris khan");
+
+    }
+}
+
+ 
+using System.Threading.Tasks;
+using System.Text;
+using System;
+ 
+ public class methods
+{
+
+    public static void Main(string[] args)
+    {
+        string message;
+
+    //Console.WriteLine("haris khan");
+Player1("my name is haris ");
+
+// Console.WriteLine("enter your name");
+// string name= Console.ReadLine();
+// Console.WriteLine(name);
+
+    }
+    
+    private static void Player1(string message)
+    {
+    
+        Console.WriteLine(message);
+    }
+}
---
+++ unity.cs
@@ -0,0 +1,63 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# OnlineCompiler
+using System.Threading.Tasks;
+using System.Text;
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        //Console.WriteLine ("haris khan the best");
+        
+        
+        
+        // string[] array= {"haris","rehan","maria"};
+        
+        // for(int i=0;i<array.Length;i++)
+        // {
+            
+        //     Console.WriteLine (array[i]);
+            
+        //     if (array[i]=="rehan")
+        //     {
+        //         break;
+        //     }
+            
+        // }
+        // //an alternate way of iterating through arrays
+        
+        // foreach(string name in array)
+        // {
+        //     Console.WriteLine(name);
+        // }
+        
+         //converting a string into a character array
+         
+        //  string name="this is haris khan";
+        //  char[] split= name.ToCharArray();
+         
+        //  Array.Reverse(split);
+         
+        //  foreach(char haris in split)
+        //  {
+             
+        //      Console.Write(haris);
+        //  }
+        
+     
+     
+     
+     
+     
+        
+
+    }//the above is a method 
+    //method is block of code that has a name
+    
+}
+        
+    
+
+
+
